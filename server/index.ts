aaedd4237d4d22e8d440b8bdfd37c62abbe06d53
greator25 <gunasekaran@greatorsoftware.com>
2025-08-29 15:41:22 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import { customErrorHandler, handle404 } from "./middleware/error-handler";
+console.log('NODE_ENV:', process.env.NODE_ENV);
 
 const app = express();
 
