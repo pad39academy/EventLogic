42c2f0212bc3faad706a07a3f72ed23d60a27afc
greator25 <gunasekaran@greatorsoftware.com>
2025-08-31 20:47:35 +0530

    Update viteProd.ts

--- viteProd.ts
+++ viteProd.ts
@@ -3,7 +3,7 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(import.meta.dirname, "public");
+  const distPath = path.resolve(import.meta.dirname, "dist");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
